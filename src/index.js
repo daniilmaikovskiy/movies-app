9dbc27f36350fbc8de36c1e6f32d0d1982a5a121
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-02 00:05:13 +0300

    test

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 
+const uro = 77;
+console.log(uro);
+
 ReactDOM.render(<App />, document.getElementById('root'));
