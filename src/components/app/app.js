e34c2bad81fb2c6ade3b0ba1c1c9e57f6735bb1e
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-02 13:54:12 +0300

    movies service

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './app.css';
+import MoviesService from '../../services/movies-service';
 
 export default class App extends React.PureComponent {
+  moviesService = new MoviesService();
+
+  onClick = () => {
+    this.moviesService.getMovies('return');
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <button type="button" onClick={this.onClick}>
+        Get Movies
+      </button>
+    );
   }
 }
---
+++ src/services/movies-service.js
@@ -0,0 +1,13 @@
+export default class MoviesService {
+  apiKey = '639828d50e7b3c39e3270402fcde3061';
+
+  url = 'https://api.themoviedb.org/3';
+
+  getMovies(query) {
+    if ((typeof query === 'string' || query instanceof String) && query.length > 0) {
+      fetch(new URL(`${this.url}/search/movie?api_key=${this.apiKey}&query=${query}`))
+        .then((responce) => responce.json())
+        .then((json) => console.log(json));
+    }
+  }
+}
