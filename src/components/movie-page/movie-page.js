57443e0330b4935200d6ba6fb328a179f17b3b76
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-05 21:07:08 +0300

    search added

--- src/components/movie-page/movie-page.js
+++ src/components/movie-page/movie-page.js
@@ -5,12 +5,17 @@ import MovieBlock from '../movie-block';
 import MoviesService from '../../services/movies-service';
 
 export default class MoviePage extends React.Component {
-  moviesService = new MoviesService();
+  moviesService = new MoviesService((newData, newTotalPages) => {
+    this.setState({
+      movieBlocksData: newData,
+      totalPages: newTotalPages,
+    });
+  });
 
   state = {
     movieBlocksData: [],
     totalPages: 0,
-    query: 'return',
+    query: '',
   };
 
   constructor() {
@@ -18,27 +23,43 @@ export default class MoviePage extends React.Component {
 
     const { query } = this.state;
 
-    this.moviesService.getMovies(query, this.updateData);
+    this.moviesService.getMovies(query);
   }
 
-  onChange = (page) => {
+  onChangePage = (page) => {
     const { query } = this.state;
 
     if (page > 0) {
-      this.moviesService.getMovies(query, this.updateData, page);
+      this.moviesService.getMovies(query, page);
     }
   };
 
-  updateData = (newData, newTotalPages) => {
-    this.setState({
-      movieBlocksData: newData,
-      totalPages: newTotalPages,
-    });
+  onChangeQuery = ({ target }) => {
+    const query = target.value.trimLeft();
+
+    this.setState({ query });
+    this.moviesService.getMovies(query);
+  };
+
+  MoviePagination = ({ total, onChange }) => {
+    if (total === 0) {
+      return null;
+    }
+
+    return (
+      <Pagination
+        size="small"
+        pageSize="1"
+        total={total}
+        showSizeChanger={false}
+        onChange={onChange}
+      />
+    );
   };
 
   render() {
-    const { movieBlocksData, totalPages } = this.state;
-    const { onChange } = this;
+    const { movieBlocksData, totalPages, query } = this.state;
+    const { onChangePage, onChangeQuery, MoviePagination } = this;
 
     const movieBlocks = movieBlocksData.map((el) => {
       const { id, ...movieData } = el;
@@ -48,14 +69,15 @@ export default class MoviePage extends React.Component {
 
     return (
       <div className="movie-page">
-        <div className="movies">{movieBlocks}</div>
-        <Pagination
-          size="small"
-          pageSize="1"
-          total={totalPages}
-          showSizeChanger={false}
-          onChange={onChange}
+        <input
+          type="search"
+          className="movie-search"
+          placeholder="Type to search..."
+          onChange={onChangeQuery}
+          value={query}
         />
+        <div className="movies">{movieBlocks}</div>
+        <MoviePagination total={totalPages} onChange={onChangePage} />
       </div>
     );
   }
--- src/services/movies-service.js
+++ src/services/movies-service.js
@@ -1,3 +1,15 @@
+const debounce = (fn, debounceTime) => {
+  let timerId = null;
+
+  return function (...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      fn.apply(this, args);
+    }, debounceTime);
+  };
+};
+
 export default class MoviesService {
   apiKey = '639828d50e7b3c39e3270402fcde3061';
 
@@ -5,32 +17,42 @@ export default class MoviesService {
 
   urlImageDB = 'https://image.tmdb.org/t/p/w500';
 
-  createURL(query, page) {
-    return `${this.url}/search/movie?api_key=${this.apiKey}&query=${query}&page=${page}`;
+  constructor(fnGet) {
+    this.fnGet = fnGet;
   }
 
-  getMovies(query, updateDataFn, page = 1) {
-    if ((typeof query === 'string' || query instanceof String) && query.length > 0) {
-      fetch(this.createURL(query, page))
-        .then((responce) => responce.json())
-        .then(({ results, total_pages: totalPages }) => {
-          const moviesData = results.map((el) => {
-            const img = el.poster_path == null ? '' : this.urlImageDB + el.poster_path;
+  createURL(query, page) {
+    const encodedQuery = encodeURIComponent(query);
 
-            const movieData = {
-              id: el.id,
-              title: el.title,
-              overview: el.overview,
-              date: el.release_date,
-              img,
-              vote: `${el.vote_average}`,
-            };
+    return `${this.url}/search/movie?api_key=${this.apiKey}&query=${encodedQuery}&page=${page}`;
+  }
 
-            return movieData;
-          });
+  debouncedGetMoviesFn = debounce((query, page) => {
+    fetch(this.createURL(query, page))
+      .then((responce) => responce.json())
+      .then(({ results, total_pages: totalPages }) => {
+        const moviesData = results.map((el) => {
+          const img = el.poster_path == null ? '' : this.urlImageDB + el.poster_path;
 
-          updateDataFn(moviesData, totalPages);
+          const movieData = {
+            id: el.id,
+            title: el.title,
+            overview: el.overview,
+            date: el.release_date,
+            img,
+            vote: `${el.vote_average}`,
+          };
+
+          return movieData;
         });
+
+        this.fnGet(moviesData, totalPages);
+      });
+  }, 1000);
+
+  getMovies(query, page = 1) {
+    if ((typeof query === 'string' || query instanceof String) && query.length > 0) {
+      this.debouncedGetMoviesFn(query, page);
     }
   }
 }
